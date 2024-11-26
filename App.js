45de2ce7e38945b9bffa2f60873c7cfe470c0331
Jenny Nguyen <jennayx14@yahoo.com>
2024-11-25 16:28:23 -0800

    corrected files in package json

--- App.js
+++ App.js
@@ -9,7 +9,6 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 
-
 const app = express();
 app.use(cors({
     credentials: true,
